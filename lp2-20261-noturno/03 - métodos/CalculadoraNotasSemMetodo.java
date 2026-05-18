8e42be213551671a7fda69d7528f04d50f0090f9
Hiago Fernandes <hiagofernands@gmail.com>
2026-05-18 12:11:23 -0300

    Update and rename SemMetodo.java to CalculadoraNotasSemMetodo.java

--- lp2-20261-noturno/03 - métodos/CalculadoraNotasSemMetodo.java
+++ lp2-20261-noturno/03 - métodos/CalculadoraNotasSemMetodo.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CalculadoraNotas {
+public class CalculadoraNotasSemMetodo {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);

e6822bf927e4d6a8529173649fede108297d5917
Hiago Fernandes <hiagofernands@gmail.com>
2026-07-14 15:02:03 -0300

    Create Gato.java

---
+++ 07 - herança e polimorfismo/exemplo_animal/Gato.java
@@ -0,0 +1,11 @@
+public class Gato extends Animal {
+
+    public Gato(String nome, int idade) {
+        super(nome, idade);
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println(getNome() + " está miando: Miau!");
+    }
+}
